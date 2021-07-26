4f428900aeb5d06f12bf2e49e8dd87ea68b783a6
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-07-26 23:49:57 +0800

    create db session

---
+++ app/db/session.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from app.core import config
+
+engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
