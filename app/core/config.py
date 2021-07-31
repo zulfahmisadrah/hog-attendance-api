32150d5a485560237f8188fa75d01dbe9541a3b8
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-07-31 20:50:45 +0800

    edit config

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,7 @@
+import secrets
+from typing import List, Union
+
+from pydantic import BaseSettings, AnyHttpUrl, validator, EmailStr
 from starlette.config import Config
 from starlette.datastructures import Secret
 
@@ -5,17 +9,43 @@ config = Config(".env")
 
 
 PROJECT_NAME = config("APP_NAME")
-SQLALCHEMY_DATABASE_URI = config("DATABASE_URL")
+SQLALCHEMY_DATABASE_URI = config("DB_URL")
 
 API_PREFIX = "/api"
 
 DEBUG = config("DEBUG", cast=bool, default=False)
 
-DB_URL: str = config("DATABASE_URL")
-HOST: str = config("HOST", default="127.0.0.1")
-PORT: int = config("PORT", cast=int, default=3306)
-USERNAME: str = config("USERNAME", default="root")
-PASSWORD: str = config("PASSWORD", default="")
-DB_NAME: str = config("DATABASE_NAME", default="")
+DB_URL: str = config("DB_URL")
+HOST: str = config("DB_HOST", default="127.0.0.1")
+PORT: int = config("DB_PORT", cast=int, default=3306)
+USERNAME: str = config("DB_USERNAME", default="root")
+PASSWORD: str = config("DB_PASSWORD", default="")
+DB_NAME: str = config("DB_NAME", default="")
 
 SECRET_KEY: Secret = config("SECRET_KEY", cast=Secret)
+
+
+class Settings(BaseSettings):
+    API_PREFIX: str = "/api"
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 120
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ['http://localhost:3000']
+
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+
+    FIRST_SUPERUSER: str = "sadmin"
+    FIRST_SUPERUSER_PASSWORD: str = "123456"
+    USERS_OPEN_REGISTRATION: bool = False
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
