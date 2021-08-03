41bcc842b46cf59fe298bcd2b18e916b356dbb6e
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-08-03 23:08:31 +0800

    adjust SQLALCHEMY_DATABASE_URI

--- app/db/session.py
+++ app/db/session.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from app.core import config
+from app.core.config import settings
 
-engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URI)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 Base = declarative_base()
