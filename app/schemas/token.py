9819158d70d1de9ebd0039dd18103d78b5ae2312
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-08-05 21:02:29 +0800

    create token schemas

---
+++ app/schemas/token.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class Token(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str
+
+
+class TokenPayload(BaseModel):
+    sub: Optional[str] = None
