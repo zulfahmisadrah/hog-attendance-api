25c95587b5583382511efdc4f834f3eb99c2dfd9
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-08-07 19:54:58 +0800

    update db base_class

--- app/db/base_class.py
+++ app/db/base_class.py
@@ -1,17 +1,7 @@
-import re
 from typing import Any
+
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
-
-# def pluralize(word):
-#     if re.search('[sxz]$', word):
-#         return re.sub('$', 'es', word)
-#     elif re.search('[^aeioudgkprt]h$', word):
-#         return re.sub('$', 'es', word)
-#     elif re.search('[aeiout]y$', word):
-#         return re.sub('y$', 'ies', word)
-#     else:
-#         return word + 's'
 
 @as_declarative()
 class Base:
@@ -22,4 +12,3 @@ class Base:
     @declared_attr
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
-        # return pluralize(cls.__name__.lower())
