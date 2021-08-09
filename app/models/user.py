a550b799b9fbbff316d52350b1f5dfc8928504c3
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-08-09 21:42:23 +0800

    update user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,17 +1,15 @@
 from sqlalchemy import Column, BigInteger, String, Boolean
 
 from app.db.base_class import Base
+from .core import CommonModel
 
 
-class User(Base):
-    # __tablename__ = "users"
-
-    id = Column(BigInteger, primary_key=True, index=True)
+class User(Base, CommonModel):
     username = Column(String(50), nullable=False, index=True)
     password = Column(String(100), nullable=False)
     name = Column(String(100), nullable=False)
     email = Column(String(100))
     phone_number = Column(String(15))
     avatar = Column(String(255))
-    is_active = Column(Boolean, default=True)
-    is_superuser = Column(Boolean, default=False)
+    is_active = Column(Boolean(), default=True)
+    is_superuser = Column(Boolean(), default=False)
