598768e0c58563185efdc9e18c75cdaf59508b5c
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-08-13 21:48:12 +0800

    update course model

--- app/models/course.py
+++ app/models/course.py
@@ -1,6 +1,7 @@
+from enum import Enum
 from typing import TYPE_CHECKING
 
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, BigInteger, Enum as SQLEnum
 from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base
@@ -8,11 +9,27 @@ from .core import CommonModel
 
 if TYPE_CHECKING:
     from .department import Department
+    from .meeting import Meeting
+    from .course_student import CourseStudent
+    from .course_lecturer import CourseLecturer
+
+
+class CourseType(str, Enum):
+    OBLIGATORY = "Wajib"
+    OPTIONAL = "Pilihan"
 
 
 class Course(Base, CommonModel):
     name = Column(String(50), nullable=False)
+    code = Column(String(15), unique=True, nullable=False)
     sks = Column(Integer, nullable=False)
-    schedule = Column(String(20), nullable=False)
-    department_id = Column(Integer, ForeignKey("department.id"))
+    semester = Column(Integer, nullable=False)
+    quota = Column(Integer, nullable=False)
+    type = Column(SQLEnum(CourseType), default=CourseType.OBLIGATORY)
+
+    department_id = Column(BigInteger, ForeignKey("department.id"))
     department = relationship("Department", back_populates="courses")
+
+    meetings = relationship("Meeting", back_populates="course")
+    students = relationship("CourseStudent", back_populates="course")
+    lecturers = relationship("CourseLecturer", back_populates="course")
