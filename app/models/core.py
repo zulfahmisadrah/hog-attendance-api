c6371ae0535e01c63cc88c34a8502a2b46063493
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-08-13 21:48:59 +0800

    update model core

--- app/models/core.py
+++ app/models/core.py
@@ -1,14 +1,48 @@
-from sqlalchemy import Column, Integer, DateTime, func
+from sqlalchemy import Column, BigInteger, DateTime, func, ForeignKey
+from sqlalchemy.orm import declared_attr
 
 
 class IdMixin(object):
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(BigInteger, autoincrement=True, primary_key=True, index=True)
 
 
 class TimeStampMixin(object):
-    created_at = Column(DateTime, default=func.now())
-    updated_at = Column(DateTime, default=func.now())
+    @declared_attr
+    def created_at(cls):
+        return Column(DateTime, default=func.now(), nullable=False)
+
+    @declared_attr
+    def updated_at(cls):
+        return Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
+
+
+class SoftDeleteMixin(object):
+    @declared_attr
+    def deleted_at(cls):
+        return Column(DateTime)
+
+    @declared_attr
+    def deleted_by(cls):
+        return Column(BigInteger, ForeignKey("user.id"))
+
+
+class TimeStampByUserMixin(object):
+    @declared_attr
+    def created_at(cls):
+        return Column(DateTime, default=func.now(), nullable=False)
+
+    @declared_attr
+    def updated_at(cls):
+        return Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
+
+    @declared_attr
+    def updated_by(cls):
+        return Column(BigInteger, ForeignKey("user.id"))
 
 
 class CommonModel(IdMixin, TimeStampMixin):
+    pass
+
+
+class BlameModel(IdMixin, TimeStampByUserMixin):
     pass
