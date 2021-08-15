adce46de538113c9ba9fbf62160d6f5fe6e4446e
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-08-15 13:47:50 +0800

    add and update some init files

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -1,1 +1,3 @@
 from .crud_user import user
+from .crud_faculty import faculty
+from .crud_department import department
--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,2 +1,5 @@
 from .user import User, UserCreate, UserUpdate
+from .role import Role, RoleCreate, RoleUpdate
+from .faculty import Faculty, FacultyCreate, FacultyUpdate, FacultyDepartments
+from .department import Department, DepartmentCreate, DepartmentUpdate, DepartmentCourses
 from .token import Token, TokenPayload
