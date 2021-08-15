84398974e2949ef5487b4ef3d87b6d4a721d3cad
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-08-15 13:49:05 +0800

    rename remove to delete

--- app/crud/base.py
+++ app/crud/base.py
@@ -49,7 +49,7 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         db.refresh(db_obj)
         return db_obj
 
-    def remove(self, db: Session, *, id: int) -> ModelType:
+    def delete(self, db: Session, *, id: int) -> ModelType:
         obj = db.query(self.model).get(id)
         db.delete(obj)
         db.commit()
