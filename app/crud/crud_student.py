a0ef262497f714995f19e891b423377b0fceecdc
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-08-27 23:21:04 +0800

    update crud_student

--- app/crud/crud_student.py
+++ app/crud/crud_student.py
@@ -9,6 +9,7 @@ from app import crud
 from app.crud.base import CRUDBase
 from app.models.domains import Student, User
 from app.models.schemas.student import StudentCreate, StudentUpdate
+from app.resources import strings
 
 
 class CRUDStudent(CRUDBase[Student, StudentCreate, StudentUpdate]):
@@ -19,19 +20,35 @@ class CRUDStudent(CRUDBase[Student, StudentCreate, StudentUpdate]):
             obj_in_data["user"]["password"] = hashed_password
         try:
             new_user = User(**obj_in_data["user"])
-            role_student = crud.role.get(db, id=2)
+            role_student = crud.role.get(db, id=4)
             new_user.roles.append(role_student)
-            new_student = Student(user_id=new_user.id, year=obj_in_data["year"], department_id=obj_in_data["department_id"])
+            del obj_in_data["user"]
+            new_student = Student(user=new_user, **obj_in_data)
             new_student.user = new_user
-            db.add(new_user)
             db.add(new_student)
             db.commit()
-            db.refresh(new_user)
+            db.refresh(new_student)
         except SQLAlchemyError as e:
             print(e.args)
             db.rollback()
-            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error Creating User")
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                detail=strings.ERROR_INTERNAL_SERVER_ERROR)
         return new_student
+
+    def update(self, db: Session, *, db_obj: Student, obj_in: StudentUpdate) -> Student:
+        update_data = obj_in.dict(exclude_unset=True)
+        update_user_data = update_data["user"]
+        if update_user_data:
+            if update_user_data["password"]:
+                hashed_password = get_password_hash(update_user_data["password"])
+                update_user_data["password"] = hashed_password
+            obj_data = jsonable_encoder(update_user_data)
+            for field in obj_data:
+                if field in update_user_data:
+                    setattr(db_obj.user, field, update_user_data[field])
+            db.add(db_obj.user)
+        del update_data["user"]
+        return super().update(db, db_obj=db_obj, obj_in=update_data)
 
 
 student = CRUDStudent(Student)
