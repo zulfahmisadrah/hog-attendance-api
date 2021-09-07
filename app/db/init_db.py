755afbe3b541f6326a5a51ac900f92eb3b38a2fc
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-09-07 12:36:43 +0800

    init_db : use DBSeeder

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -1,39 +1,27 @@
-import json
 from sqlalchemy.orm import Session
 
+from app import crud
+from app.models import schemas
 from app.core.config import settings
-from app.crud import crud_user, crud_role, crud_faculty, crud_department
-from app.models.schemas import UserCreate, RoleCreate, FacultyCreate, DepartmentCreate
+from app.db.db_seeder import DBSeeder
+from app.resources.enums import RoleEnum
 
 
 def init_db(db: Session) -> None:
+    DBSeeder(db, crud.role, schemas.RoleCreate).load_from_json("role.json")
 
-    json_file_role = open(settings.INITIAL_DATA_FOLDER + "role.json")
-    for data_dict in json.load(json_file_role):
-        data = crud_role.role.get(db, id=data_dict.get("id"))
-        if not data:
-            obj_in = RoleCreate(**data_dict)
-            crud_role.role.create(db, obj_in=obj_in)
-
-    user = crud_user.user.get_by_username(db, username=settings.FIRST_SUPERUSER_USERNAME)
+    user = crud.user.get_by_username(db, username=settings.FIRST_SUPERUSER_USERNAME)
     if not user:
-        user_in = UserCreate(
+        user_in = schemas.UserCreate(
             name=settings.FIRST_SUPERUSER_NAME,
             username=settings.FIRST_SUPERUSER_USERNAME,
             password=settings.FIRST_SUPERUSER_PASSWORD,
+            roles=[{"code": RoleEnum.SUPER_USER.value}]
         )
-        user = crud_user.user.create_superuser(db, obj_in=user_in)
+        crud.user.create(db, obj_in=user_in)
 
-    json_file_faculty = open(settings.INITIAL_DATA_FOLDER + "faculty.json")
-    for data_dict in json.load(json_file_faculty):
-        data = crud_faculty.faculty.get(db, id=data_dict.get("id"))
-        if not data:
-            obj_in = FacultyCreate(**data_dict)
-            crud_faculty.faculty.create(db, obj_in=obj_in)
-
-    json_file_department = open(settings.INITIAL_DATA_FOLDER + "department.json")
-    for data_dict in json.load(json_file_department):
-        data = crud_department.department.get(db, id=data_dict.get("id"))
-        if not data:
-            obj_in = DepartmentCreate(**data_dict)
-            crud_department.department.create(db, obj_in=obj_in)
+    DBSeeder(db, crud.user, schemas.UserCreate).load_from_json("user.json")
+    DBSeeder(db, crud.lecturer, schemas.LecturerCreate).load_from_json("lecturer.json")
+    DBSeeder(db, crud.student, schemas.StudentCreate).load_from_json("student.json")
+    DBSeeder(db, crud.faculty, schemas.FacultyCreate).load_from_json("faculty.json")
+    DBSeeder(db, crud.department, schemas.DepartmentCreate).load_from_json("department.json")
