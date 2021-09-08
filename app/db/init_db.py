dba23b8a6102bd486c2507c273ee3d0da1d7eb90
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-09-09 00:39:29 +0800

    init_db : rearrange and clean code

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -2,30 +2,17 @@ from sqlalchemy.orm import Session
 
 from app import crud
 from app.models import schemas
-from app.core.config import settings
 from app.db.db_seeder import DBSeeder
-from app.resources.enums import RoleEnum
 
 
 def init_db(db: Session) -> None:
     DBSeeder(db, crud.role, schemas.RoleCreate).load_from_json("role.json")
-
-    user = crud.user.get_by_username(db, username=settings.FIRST_SUPERUSER_USERNAME)
-    if not user:
-        user_in = schemas.UserCreate(
-            name=settings.FIRST_SUPERUSER_NAME,
-            username=settings.FIRST_SUPERUSER_USERNAME,
-            password=settings.FIRST_SUPERUSER_PASSWORD,
-            roles=[{"code": RoleEnum.SUPER_USER.value}]
-        )
-        crud.user.create(db, obj_in=user_in)
-
-    DBSeeder(db, crud.user, schemas.UserCreate).load_from_json("user.json")
-    DBSeeder(db, crud.lecturer, schemas.LecturerCreate).load_from_json("lecturer.json")
-    DBSeeder(db, crud.student, schemas.StudentCreate).load_from_json("student.json")
     DBSeeder(db, crud.faculty, schemas.FacultyCreate).load_from_json("faculty.json")
     DBSeeder(db, crud.department, schemas.DepartmentCreate).load_from_json("department.json")
     DBSeeder(db, crud.semester, schemas.SemesterCreate).load_from_json("semester.json")
+    DBSeeder(db, crud.user, schemas.UserRolesCreate).load_from_json("user.json")
+    DBSeeder(db, crud.lecturer, schemas.UserLecturerCreate).load_from_json("lecturer.json")
+    DBSeeder(db, crud.student, schemas.UserStudentCreate).load_from_json("student.json")
     DBSeeder(db, crud.schedule, schemas.ScheduleCreate).load_from_json("schedule.json")
     DBSeeder(db, crud.course, schemas.CourseCreate).load_from_json("course.json")
     DBSeeder(db, crud.meeting, schemas.MeetingCreate).load_from_json("meeting.json")
