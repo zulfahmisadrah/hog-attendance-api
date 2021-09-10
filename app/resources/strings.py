d01440f6c5a9a351eaef83ed8089648f38af54cc
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-09-10 22:57:50 +0800

    add strings

--- app/resources/strings.py
+++ app/resources/strings.py
@@ -1,7 +1,11 @@
 USERNAME_TAKEN = "Username telah digunakan"
 ERROR_DATA_NOT_FOUND = "Data tidak ditemukan"
 ERROR_DATA_ID_NOT_EXIST = "Data dengan id {0} tidak ditemukan"
+ERROR_MODEL_ID_NOT_EXIST = "{0} dengan id {1} tidak ditemukan"
 ERROR_FACULTY_NAME_CODE_EXIST = "Nama atau kode fakultas telah digunakan"
 SUCCESS_DATA_DELETED = "Data berhasil dihapus"
 ERROR_INTERNAL_SERVER_ERROR = "Terjadi Kesalahan"
 ROLE_INVALID = "Role tidak valid"
+
+MODEL_LECTURER = "Dosen"
+MODEL_STUDENT = "Mahasiswa"
