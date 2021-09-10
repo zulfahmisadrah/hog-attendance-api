f6552fd82aca88ba8f75cc3472d122c8f48dd91c
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-09-10 23:10:37 +0800

    crud_semester : add function get_active_semester

--- app/crud/crud_semester.py
+++ app/crud/crud_semester.py
@@ -1,10 +1,13 @@
+from sqlalchemy.orm import Session
+
 from app.crud.base import CRUDBase
 from app.models.domains import Semester
 from app.models.schemas.semester import SemesterCreate, SemesterUpdate
 
 
 class CRUDSemester(CRUDBase[Semester, SemesterCreate, SemesterUpdate]):
-    pass
+    def get_active_semester(self, db: Session):
+        return db.query(Semester).filter(Semester.is_active == 1).first()
 
 
 semester = CRUDSemester(Semester)
