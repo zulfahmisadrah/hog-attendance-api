dafcd88633b4baf901dbbbe42d45f608c022c249
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-09-12 02:10:51 +0800

    api/lecturers: add function get_lecturer_courses, optimize function get_lecturer, update response_model

--- app/api/endpoints/lecturers.py
+++ app/api/endpoints/lecturers.py
@@ -8,11 +8,12 @@ from app.models import schemas
 from app.api import deps
 from app.db import session
 from app.resources import strings
+from app.api.endpoints.semesters import get_semester
 
 router = APIRouter()
 
 
-@router.get("/", response_model=List[schemas.Lecturer], dependencies=[Depends(deps.get_current_active_user)])
+@router.get("/", response_model=List[schemas.LecturerUser], dependencies=[Depends(deps.get_current_active_user)])
 def get_list_lecturers(db: Session = Depends(session.get_db), offset: int = 0, limit: int = 20):
     list_data = crud.lecturer.get_list(db, offset=offset, limit=limit)
     return list_data
@@ -27,15 +28,18 @@ def create_lecturer(user_in: schemas.UserLecturerCreate, db: Session = Depends(d
     return crud.lecturer.create(db, obj_in=user_in)
 
 
-@router.get("/{lecturer_id}", response_model=schemas.Lecturer, dependencies=[Depends(deps.get_current_active_user)])
+@router.get("/{lecturer_id}", response_model=schemas.LecturerUser, dependencies=[Depends(deps.get_current_active_user)])
 def get_lecturer(lecturer_id: int, db: Session = Depends(session.get_db)):
-    data = crud.lecturer.get(db, lecturer_id)
-    if not data:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=strings.ERROR_DATA_NOT_FOUND)
-    return data
+    lecturer = crud.lecturer.get(db, lecturer_id)
+    if not lecturer:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=strings.ERROR_MODEL_ID_NOT_EXIST.format(strings.MODEL_LECTURER, lecturer_id)
+        )
+    return lecturer
 
 
-@router.put('/{lecturer_id}', response_model=schemas.Lecturer, dependencies=[Depends(deps.get_current_admin)])
+@router.put('/{lecturer_id}', response_model=schemas.LecturerUser, dependencies=[Depends(deps.get_current_admin)])
 def update_lecturer(lecturer_id: int, lecturer: schemas.LecturerUpdate, db: Session = Depends(session.get_db)):
     db_obj = crud.lecturer.get(db, lecturer_id)
     if db_obj is None:
@@ -50,3 +54,20 @@ def delete_lecturer(lecturer_id: int, db: Session = Depends(session.get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=strings.ERROR_DATA_ID_NOT_EXIST.format(lecturer_id))
     return crud.lecturer.delete(db=db, id=lecturer_id)
+
+
+@router.get("/{lecturer_id}/courses", response_model=schemas.LecturerCourses,
+            dependencies=[Depends(deps.get_current_active_user)])
+def get_lecturer_courses(lecturer_id: int, semester_id: int = 0, db: Session = Depends(session.get_db)):
+    lecturer = get_lecturer(lecturer_id, db)
+    if semester_id == 0:
+        semester = crud.semester.get_active_semester(db)
+    else:
+        semester = get_semester(semester_id, db)
+    data = crud.lecturer.get_lecturer_courses(db, lecturer_id=lecturer_id, semester_id=semester_id)
+    list_courses = []
+    for lecturer_course in data:
+        course = schemas.Course.from_orm(lecturer_course.course)
+        list_courses.append(course)
+    lecturer_courses = schemas.LecturerCourses(semester=semester, lecturer=lecturer, courses=list_courses)
+    return lecturer_courses
