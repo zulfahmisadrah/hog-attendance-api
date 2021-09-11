f4666bdc90ceb1c810526c34a8e3730ddafaac17
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-09-12 02:05:27 +0800

    api/semesters: optimize get_semester and clean up code

--- app/api/endpoints/semesters.py
+++ app/api/endpoints/semesters.py
@@ -1,5 +1,5 @@
 from typing import List
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from app import crud
@@ -18,20 +18,23 @@ def get_list_semesters(db: Session = Depends(session.get_db), offset: int = 0, l
 
 
 @router.post("/", response_model=schemas.Semester, status_code=201, dependencies=[Depends(deps.get_current_admin)])
-def create_semester(semester: schemas.SemesterCreate, db: Session = Depends(session.get_db),):
+def create_semester(semester: schemas.SemesterCreate, db: Session = Depends(session.get_db)):
     return crud.semester.create(db=db, obj_in=semester)
 
 
 @router.get("/{semester_id}", response_model=schemas.Semester, dependencies=[Depends(deps.get_current_active_user)])
 def get_semester(semester_id: int, db: Session = Depends(session.get_db)):
-    data = crud.semester.get(db, semester_id)
-    if not data:
-        raise HTTPException(status_code=404, detail=strings.ERROR_DATA_NOT_FOUND)
-    return data
+    semester = crud.semester.get(db, semester_id)
+    if not semester:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=strings.ERROR_MODEL_ID_NOT_EXIST.format(strings.MODEL_SEMESTER, semester_id)
+        )
+    return semester
 
 
 @router.put('/{semester_id}', response_model=schemas.Semester, dependencies=[Depends(deps.get_current_admin)])
-def update_semester(semester_id: int, semester: schemas.SemesterUpdate, db: Session = Depends(session.get_db),):
+def update_semester(semester_id: int, semester: schemas.SemesterUpdate, db: Session = Depends(session.get_db)):
     db_obj = crud.semester.get(db, semester_id)
     if db_obj is None:
         raise HTTPException(status_code=404, detail=strings.ERROR_DATA_NOT_FOUND)
@@ -44,4 +47,3 @@ def delete_semester(semester_id: int, db: Session = Depends(session.get_db)):
     if db_obj is None:
         raise HTTPException(status_code=404, detail=strings.ERROR_DATA_ID_NOT_EXIST.format(semester_id))
     return crud.semester.delete(db=db, id=semester_id)
-
