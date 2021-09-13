460ba0a7557a488ab7c8d6ccd83fc182165fea81
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-09-13 22:12:51 +0800

    api/meetings :  create endpoints /me to get current user meetings

--- app/api/endpoints/meetings.py
+++ app/api/endpoints/meetings.py
@@ -1,11 +1,11 @@
-from typing import List
+from typing import List, Union
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from app import crud
-from app.models import domains, schemas
 from app.api import deps
 from app.db import session
+from app.models import schemas
 from app.resources import strings
 
 router = APIRouter()
@@ -17,9 +17,29 @@ def get_list_meetings(db: Session = Depends(session.get_db), offset: int = 0, li
     return list_data
 
 
-@router.post("/", response_model=schemas.Meeting, status_code=status.HTTP_201_CREATED, dependencies=[Depends(deps.get_current_admin)])
+@router.post("/", response_model=schemas.Meeting, status_code=status.HTTP_201_CREATED,
+             dependencies=[Depends(deps.get_current_admin)])
 def create_meeting(meeting: schemas.MeetingCreate, db: Session = Depends(session.get_db)):
     return crud.meeting.create(db=db, obj_in=meeting)
+
+
+@router.get("/me", response_model=List[schemas.Meeting])
+def get_my_meetings(
+        db: Session = Depends(session.get_db),
+        current_semester: schemas.Semester = Depends(deps.get_active_semester),
+        current_user: Union[schemas.UserLecturer, schemas.UserStudent] = Depends(deps.get_current_active_user)
+):
+    if current_user.student:
+        student_id = current_user.student.id
+        data = crud.student.get_student_courses(db, student_id=student_id, semester_id=current_semester.id)
+    else:
+        lecturer_id = current_user.lecturer.id
+        data = crud.lecturer.get_lecturer_courses(db, lecturer_id=lecturer_id, semester_id=current_semester.id)
+    list_meetings = []
+    for lecturer_course in data:
+        meetings: List[schemas.Meeting] = lecturer_course.course.meetings
+        list_meetings.extend(meetings)
+    return list_meetings
 
 
 @router.get("/{meeting_id}", response_model=schemas.Meeting, dependencies=[Depends(deps.get_current_active_user)])
@@ -42,6 +62,6 @@ def update_meeting(meeting_id: int, meeting: schemas.MeetingUpdate, db: Session 
 def delete_meeting(meeting_id: int, db: Session = Depends(session.get_db)):
     db_obj = crud.meeting.get(db, meeting_id)
     if db_obj is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=strings.ERROR_DATA_ID_NOT_EXIST.format(meeting_id))
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=strings.ERROR_DATA_ID_NOT_EXIST.format(meeting_id))
     return crud.meeting.delete(db=db, id=meeting_id)
-
