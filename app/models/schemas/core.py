25643a5bc15a7aa6596021730923587c1f00a2ea
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-09-21 17:21:04 +0800

    schemas/core : create reusable model

--- app/models/schemas/core.py
+++ app/models/schemas/core.py
@@ -1,7 +1,21 @@
 from typing import Optional
-from datetime import datetime
+from datetime import datetime, timezone
 
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, validator, BaseConfig
+
+
+def convert_datetime_to_iso_format(dt: datetime) -> str:
+    return dt.replace(tzinfo=timezone.utc).isoformat().replace("+00:00", "Z")
+
+
+class Model(BaseModel):
+    class Config(BaseConfig):
+        json_encoders = {datetime: convert_datetime_to_iso_format}
+
+
+class ModelORM(Model):
+    class Config(Model.Config):
+        orm_mode = True
 
 
 class IDMixin(BaseModel):
@@ -9,8 +23,8 @@ class IDMixin(BaseModel):
 
 
 class DateTimeModelMixin(BaseModel):
-    created_at: Optional[datetime]
-    updated_at: Optional[datetime]
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
 
     @validator("created_at", "updated_at", pre=True)
     def default_datetime(cls, value: datetime) -> datetime:
