71d19536bc194720cc60aaa604e79a901cf56db9
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-09-21 10:34:36 +0800

    file_helper : optimization

--- app/utils/file_helper.py
+++ app/utils/file_helper.py
@@ -4,16 +4,16 @@ from os import path, listdir
 from app.core.config import settings
 
 
-def get_ml_models(file_name: str):
+def get_ml_models(file_name: str) -> str:
     return path.join(settings.ML_MODELS_FOLDER, file_name)
 
 
-def get_initial_data_file(file_name: str):
+def get_initial_data_file(file_name: str) -> str:
     return path.join(settings.INITIAL_DATA_FOLDER, file_name)
 
 
 def get_list_files(directory: str) -> List:
-    return [file for file in listdir(directory)]
+    return listdir(directory)
 
 
 def get_total_files(directory: str) -> int:
