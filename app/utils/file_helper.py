8c80289e8a7f60e56f1fe73f995996a2aa841d9f
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-09-22 19:34:43 +0800

    file_helper : create and update some functions

--- app/utils/file_helper.py
+++ app/utils/file_helper.py
@@ -1,15 +1,13 @@
 from typing import List
-from os import path, listdir
+from os import path, listdir, mkdir
 
 from app.core.config import settings
 
 
-def get_ml_models(file_name: str) -> str:
-    return path.join(settings.ML_MODELS_FOLDER, file_name)
-
-
-def get_initial_data_file(file_name: str) -> str:
-    return path.join(settings.INITIAL_DATA_FOLDER, file_name)
+def create_directory_if_not_exist(directory: str) -> str:
+    if not path.isdir(directory):
+        mkdir(directory)
+    return directory
 
 
 def get_list_files(directory: str) -> List:
@@ -18,3 +16,29 @@ def get_list_files(directory: str) -> List:
 
 def get_total_files(directory: str) -> int:
     return len(get_list_files(directory))
+
+
+def get_initial_data_file(file_name: str) -> str:
+    return path.join(settings.INITIAL_DATA_FOLDER, file_name)
+
+
+def get_user_datasets_directory(username: str) -> str:
+    directory = path.join(settings.DATASETS_FOLDER, username)
+    directory = create_directory_if_not_exist(directory)
+    return directory
+
+
+def get_course_models_directory(course_code: str) -> str:
+    directory = path.join(settings.ML_MODELS_FOLDER, course_code)
+    directory = create_directory_if_not_exist(directory)
+    return directory
+
+
+def get_hog_outputs_directory(username: str) -> str:
+    directory = path.join(settings.ML_OUTPUTS_FOLDER, username)
+    directory = create_directory_if_not_exist(directory)
+    return directory
+
+
+def get_course_models_files(course_code: str) -> List:
+    return listdir(get_course_models_directory(course_code))
