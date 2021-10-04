a1f383ddc0049f7373fb4b1c6886c80ce13f8368
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-10-04 21:20:01 +0800

    crud : add functions to filter by deparment_id or name or username on student and lecturer

--- app/crud/crud_lecturer.py
+++ app/crud/crud_lecturer.py
@@ -1,15 +1,56 @@
 from fastapi.encoders import jsonable_encoder
+from sqlalchemy import or_
 from sqlalchemy.orm import Session
 
 from app import crud
 from app.crud.base import CRUDBase
-from app.models.domains import Lecturer, CourseLecturer
+from app.models.domains import User, Lecturer, CourseLecturer
 from app.core.security import get_password_hash
-from app.models.schemas import User, UserLecturerCreate
+from app.models.schemas import UserLecturerCreate, LecturerUser
 from app.models.schemas.lecturer import LecturerCreate, LecturerUpdate
 
 
 class CRUDLecturer(CRUDBase[Lecturer, LecturerCreate, LecturerUpdate]):
+    def get_by_username_or_name(self, db: Session, *, keyword: str, offset: int = 0, limit: int = 10) -> Lecturer:
+        keyword = "%{}%".format(keyword)
+        return db.query(Lecturer).join(Lecturer.user) \
+            .filter(
+            or_(
+                User.username.like(keyword),
+                User.name.like(keyword)
+            )) \
+            .offset(offset).limit(limit).all()
+
+    def get_by_name(self, db: Session, *, name: str, offset: int = 0, limit: int = 10) -> Lecturer:
+        keyword = "%{}%".format(name)
+        return db.query(Lecturer).join(Lecturer.user) \
+            .filter(User.name.like(keyword)) \
+            .offset(offset).limit(limit).all()
+
+    def get_by_department_id(self, db: Session, *, department_id: int) -> Lecturer:
+        return db.query(Lecturer).filter(Lecturer.department_id == department_id).all()
+
+    def get_by_department_id_and_username_or_name(self, db: Session, *, department_id: int, keyword: str,
+                                                  offset: int = 0, limit: int = 10) -> Lecturer:
+        keyword = "%{}%".format(keyword)
+        return db.query(Lecturer).join(Lecturer.user) \
+            .filter(
+            Lecturer.department_id == department_id,
+            or_(
+                User.username.like(keyword),
+                User.name.like(keyword)
+            )) \
+            .order_by(User.username) \
+            .offset(offset).limit(limit).all()
+
+    def get_by_department_id_and_name(self, db: Session, *, department_id: int, name: str, offset: int = 0,
+                                      limit: int = 10) -> Lecturer:
+        keyword = "%{}%".format(name)
+        return db.query(Lecturer).join(Lecturer.user) \
+            .filter(Lecturer.department_id == department_id, User.name.like(keyword)) \
+            .order_by(User.username) \
+            .offset(offset).limit(limit).all()
+
     def get_lecturer_courses(self, db: Session, *, lecturer_id: int, semester_id: int) -> CourseLecturer:
         return db.query(CourseLecturer).filter(
             CourseLecturer.semester_id == semester_id,
--- app/crud/crud_student.py
+++ app/crud/crud_student.py
@@ -1,9 +1,10 @@
 from fastapi.encoders import jsonable_encoder
+from sqlalchemy import or_
 from sqlalchemy.orm import Session
 
 from app import crud
 from app.crud.base import CRUDBase
-from app.models.domains import Student, CourseStudent
+from app.models.domains import Student, CourseStudent, User
 from app.core.security import get_password_hash
 from app.models.schemas import UserStudent
 from app.models.schemas.student import StudentCreate, StudentUpdate
@@ -11,6 +12,46 @@ from app.models.schemas.user import UserStudentCreate
 
 
 class CRUDStudent(CRUDBase[Student, StudentCreate, StudentUpdate]):
+    def get_by_username_or_name(self, db: Session, *, keyword: str, offset: int = 0, limit: int = 10) -> Student:
+        keyword = "%{}%".format(keyword)
+        return db.query(Student).join(Student.user) \
+            .filter(
+            or_(
+                User.username.like(keyword),
+                User.name.like(keyword)
+            )) \
+            .offset(offset).limit(limit).all()
+
+    def get_by_name(self, db: Session, *, name: str, offset: int = 0, limit: int = 10) -> Student:
+        keyword = "%{}%".format(name)
+        return db.query(Student).join(Student.user) \
+            .filter(User.name.like(keyword)) \
+            .offset(offset).limit(limit).all()
+
+    def get_by_department_id(self, db: Session, *, department_id: int) -> Student:
+        return db.query(Student).filter(Student.department_id == department_id).all()
+
+    def get_by_department_id_and_username_or_name(self, db: Session, *, department_id: int, keyword: str,
+                                                  offset: int = 0, limit: int = 10) -> Student:
+        keyword = "%{}%".format(keyword)
+        return db.query(Student).join(Student.user) \
+            .filter(
+            Student.department_id == department_id,
+            or_(
+                User.username.like(keyword),
+                User.name.like(keyword)
+            )) \
+            .order_by(User.username) \
+            .offset(offset).limit(limit).all()
+
+    def get_by_department_id_and_name(self, db: Session, *, department_id: int, name: str, offset: int = 0,
+                                      limit: int = 10) -> Student:
+        keyword = "%{}%".format(name)
+        return db.query(Student).join(Student.user) \
+            .filter(Student.department_id == department_id, User.name.like(keyword)) \
+            .order_by(User.username) \
+            .offset(offset).limit(limit).all()
+
     def get_student_courses(self, db: Session, *, student_id: int, semester_id: int) -> CourseStudent:
         return db.query(CourseStudent).filter(
             CourseStudent.semester_id == semester_id,
