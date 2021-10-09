ecaa17f6311bc4205510862b078a8ff8e1712a6a
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-10-10 00:19:45 +0800

    semester : update function create_semeseter, create functions create_next_semester and activate_semester

--- app/api/endpoints/semesters.py
+++ app/api/endpoints/semesters.py
@@ -6,6 +6,7 @@ from app import crud
 from app.models import schemas
 from app.api import deps
 from app.db import session
+from app.models.domains.semester import SemesterType
 from app.resources import strings
 
 router = APIRouter()
@@ -19,6 +20,10 @@ def get_list_semesters(db: Session = Depends(session.get_db), offset: int = 0, l
 
 @router.post("/", response_model=schemas.Semester, status_code=201, dependencies=[Depends(deps.get_current_admin)])
 def create_semester(semester: schemas.SemesterCreate, db: Session = Depends(session.get_db)):
+    number = 1 if semester.type == SemesterType.GANJIL else 2
+    semester.code = str(semester.year) + str(number)
+    year_start = semester.year if semester.type == SemesterType.GANJIL else semester.year - 1
+    semester.academic_year = f"{year_start}/{year_start + 1}"
     return crud.semester.create(db=db, obj_in=semester)
 
 
@@ -30,6 +35,26 @@ def get_semester(semester_id: int, db: Session = Depends(session.get_db)):
             status_code=status.HTTP_404_NOT_FOUND,
             detail=strings.ERROR_MODEL_ID_NOT_EXIST.format(strings.MODEL_SEMESTER, semester_id)
         )
+    return semester
+
+
+@router.post("/next_semester", response_model=schemas.Semester, status_code=201,
+             dependencies=[Depends(deps.get_current_admin)])
+def create_next_semester(db: Session = Depends(session.get_db)):
+    latest_semester = crud.semester.get_latest_semester(db)
+    print(latest_semester)
+    semester_in = schemas.SemesterCreate(
+        year=latest_semester.year if latest_semester.type == SemesterType.GENAP else latest_semester.year + 1,
+        type=SemesterType.GANJIL if latest_semester.type == SemesterType.GENAP else SemesterType.GENAP
+    )
+    next_semester = create_semester(semester_in, db)
+    return next_semester
+
+
+@router.post("/{semester_id}/activate", response_model=schemas.Semester, dependencies=[Depends(deps.get_current_admin)])
+def activate_semester(semester_id: int, db: Session = Depends(session.get_db)):
+    semester = get_semester(semester_id, db)
+    crud.semester.activate_semester(db, semester_id)
     return semester
 
 
--- app/crud/crud_semester.py
+++ app/crud/crud_semester.py
@@ -1,13 +1,31 @@
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 
 from app.crud.base import CRUDBase
 from app.models.domains import Semester
+from app.models.domains.semester import SemesterType
 from app.models.schemas.semester import SemesterCreate, SemesterUpdate
 
 
 class CRUDSemester(CRUDBase[Semester, SemesterCreate, SemesterUpdate]):
-    def get_active_semester(self, db: Session):
+    def get_active_semester(self, db: Session) -> Semester:
         return db.query(Semester).filter(Semester.is_active == 1).first()
+
+    def activate_semester(self, db: Session, semester_id: int) -> None:
+        current_active_semester = self.get_active_semester(db)
+        db.query(Semester).filter(Semester.id == current_active_semester.id).update({Semester.is_active: 0})
+        db.query(Semester).filter(Semester.id == semester_id).update({Semester.is_active: 1})
+        db.commit()
+
+    def get_latest_semester(self, db: Session) -> Semester:
+        latest_year = db.query(func.max(Semester.year)).scalar_subquery()
+        latest_year_semesters = db.query(Semester).filter(Semester.year == latest_year).all()
+        latest_semester = latest_year_semesters[0]
+        if len(latest_year_semesters) == 2:
+            for latest_year_semester in latest_year_semesters:
+                if latest_year_semester.type == SemesterType.GANJIL:
+                    latest_semester = latest_year_semester
+        return latest_semester
 
 
 semester = CRUDSemester(Semester)
