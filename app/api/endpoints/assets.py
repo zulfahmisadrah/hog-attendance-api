58c8a4c71cb7899be63bb190a496db41f82d95cc
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-10-16 23:53:34 +0800

    api/assets : add get_dataset

--- app/api/endpoints/assets.py
+++ app/api/endpoints/assets.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from fastapi.responses import FileResponse
 
-from app.utils.file_helper import get_avatar_file
+from app.utils.file_helper import get_avatar_file, get_user_dataset_file
 
 router = APIRouter()
 
@@ -10,3 +10,9 @@ router = APIRouter()
 def get_avatar(file_name: str):
     avatar = get_avatar_file(file_name)
     return FileResponse(avatar)
+
+
+@router.get("/dataset/{username}/{file_name}")
+def get_dataset(username: str, file_name: str):
+    dataset = get_user_dataset_file(username, file_name)
+    return FileResponse(dataset)
