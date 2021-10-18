5847643e6b388f20c20ca5259d187166cce1a1b9
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-10-18 22:23:22 +0800

    enums : add DayOfWeek and MeetingStatus

--- app/resources/enums.py
+++ app/resources/enums.py
@@ -6,3 +6,19 @@ class RoleEnum(str, Enum):
     ADMIN = "ROLE_ADMIN"
     LECTURER = "ROLE_LECTURER"
     STUDENT = "ROLE_STUDENT"
+
+
+class DayOfWeek(int, Enum):
+    Senin = 1
+    Selasa = 2
+    Rabu = 3
+    Kamis = 4
+    Jumat = 5
+    Sabtu = 6
+    Minggu = 7
+
+
+class MeetingStatus(str, Enum):
+    Terjadwal = "Terjadwal"
+    Berlangsung = "Berlangsung"
+    Selesai = "Selesai"
