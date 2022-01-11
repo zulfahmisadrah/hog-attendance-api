3867e602cb8b265241a94c549898527462bcebdd
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2022-01-11 19:27:15 +0800

    datasets_training: add facenet training

--- app/ml/datasets_training.py
+++ app/ml/datasets_training.py
@@ -1,26 +1,28 @@
 import cv2
 import joblib
 from os import path
+
+from tqdm import tqdm
+from sklearn.svm import SVC
 from sklearn.metrics import classification_report
 from sklearn.model_selection import GridSearchCV
-from sklearn.svm import SVC
 
 from app.core.config import settings
 from app.resources.enums import DatasetType
-from app.services.image_processing import get_hog_features, enhance_image, convert_to_grayscale
+from app.services.image_processing import get_hog_features, enhance_image, convert_to_grayscale, get_embedding, \
+    create_scatter_plot
 from app.utils.file_helper import get_list_files, get_course_models_directory, get_extracted_images_directory, \
     get_user_datasets_directory, get_datasets_directory, get_user_preprocessed_images_directory, \
     get_file_name_without_extension
 
 
-def prepare_datasets(dataset_type: DatasetType = DatasetType.TRAINING, save_preprocessing: bool = False):
-    print('--- PREPARING DATASETS ---')
-
-    images = []
+def prepare_datasets(dataset_type: DatasetType = DatasetType.TRAINING, save_preprocessing: bool = False,
+                     use_facenet: bool = settings.USE_FACENET):
+    features = []
     labels = []
     datasets = get_list_files(get_datasets_directory(dataset_type))
     total_labels = 0
-    for username in datasets:
+    for user_index, username in enumerate(tqdm(datasets)):
         user_datasets_dir = get_user_datasets_directory(dataset_type, username)
         preprocessed_images_dir = get_user_preprocessed_images_directory(dataset_type, username)
         extracted_images_dir = get_extracted_images_directory(username)
@@ -36,40 +38,44 @@ def prepare_datasets(dataset_type: DatasetType = DatasetType.TRAINING, save_prep
                 image_path = path.join(user_datasets_dir, image_name)
                 image = cv2.imread(image_path)
 
-                # Image Enhancement
-                enhanced_image = enhance_image(image)
-                if save_preprocessing:
-                    enhanced_image_path = path.join(preprocessed_images_dir, f"{file_name_prefix}.4_enhanced.jpg")
-                    cv2.imwrite(enhanced_image_path, enhanced_image)
+                if use_facenet:
+                    image = cv2.resize(image, settings.FACENET_INPUT_SIZE)
+                    feature = get_embedding(image)
+                else:
+                    # Image Enhancement
+                    enhanced_image = enhance_image(image)
+                    if save_preprocessing:
+                        enhanced_image_path = path.join(preprocessed_images_dir, f"{file_name_prefix}.4_enhanced.jpg")
+                        cv2.imwrite(enhanced_image_path, enhanced_image)
 
-                # Grayscaling
-                gray_image = convert_to_grayscale(enhanced_image)
-                if save_preprocessing:
-                    gray_image_path = path.join(preprocessed_images_dir, f"{file_name_prefix}.5_gray.jpg")
-                    cv2.imwrite(gray_image_path, gray_image)
+                    # Grayscaling
+                    gray_image = convert_to_grayscale(enhanced_image)
+                    if save_preprocessing:
+                        gray_image_path = path.join(preprocessed_images_dir, f"{file_name_prefix}.5_gray.jpg")
+                        cv2.imwrite(gray_image_path, gray_image)
 
-                # Resize
-                resized_image = cv2.resize(gray_image, (settings.HOG_RESIZE_WIDTH, settings.HOG_RESIZE_HEIGHT))
-                if save_preprocessing:
-                    resized_image_path = path.join(preprocessed_images_dir, f"{file_name_prefix}.6_resized.jpg")
-                    cv2.imwrite(resized_image_path, resized_image)
+                    # Resize
+                    resized_image = cv2.resize(gray_image, (settings.HOG_RESIZE_WIDTH, settings.HOG_RESIZE_HEIGHT))
+                    if save_preprocessing:
+                        resized_image_path = path.join(preprocessed_images_dir, f"{file_name_prefix}.6_resized.jpg")
+                        cv2.imwrite(resized_image_path, resized_image)
 
-                # HOG Features
-                (hog_desc, hog_image) = get_hog_features(resized_image)
-                if save_preprocessing:
-                    hog_path = path.join(preprocessed_images_dir, f"{file_name_prefix}.7_hog.jpg")
-                    cv2.imwrite(hog_path, hog_image * 255.)
-                output_path = path.join(extracted_images_dir, f"{dataset_type}_{file_name}_hog.jpg")
-                cv2.imwrite(output_path, hog_image * 255.)
-
-                images.append(hog_desc)
+                    # HOG Features
+                    (feature, hog_image) = get_hog_features(resized_image)
+                    if save_preprocessing:
+                        hog_path = path.join(preprocessed_images_dir, f"{file_name_prefix}.7_hog.jpg")
+                        cv2.imwrite(hog_path, hog_image * 255.)
+                    output_path = path.join(extracted_images_dir, f"{dataset_type}_{file_name}_hog.jpg")
+                    cv2.imwrite(output_path, hog_image * 255.)
+                features.append(feature)
                 labels.append(username)
-    print('{0} images from {1} labels have been extracted'.format(len(images), total_labels))
-    return images, labels
+    print('{0} images from {1} labels have been extracted'.format(len(features), total_labels))
+    return features, labels
 
 
 def train_datasets(semester_code: str, course_code: str, save_preprocessing: bool = False, grid_search: bool = False):
-    images, labels = prepare_datasets(DatasetType.TRAINING, save_preprocessing)
+    print('--- PREPARING TRAINING DATASETS ---')
+    features, labels = prepare_datasets(DatasetType.TRAINING, save_preprocessing)
     print('--- TRAINING MODEL ---')
     if grid_search:
         parameters = {
@@ -79,7 +85,7 @@ def train_datasets(semester_code: str, course_code: str, save_preprocessing: boo
             'random_state': [0]
         }
         grid_search = GridSearchCV(estimator=SVC(), param_grid=parameters, n_jobs=6, verbose=1, scoring='accuracy')
-        grid_search.fit(images, labels)
+        grid_search.fit(features, labels)
         print(f"Best Score: {grid_search.best_score_}")
         best_params = grid_search.best_estimator_.get_params()
         print("Best Parameters: ")
@@ -88,20 +94,25 @@ def train_datasets(semester_code: str, course_code: str, save_preprocessing: boo
         svm_model = SVC(kernel=best_params['kernel'], gamma=best_params['gamma'], C=best_params['C'],
                         random_state=best_params['random_state'], probability=True)
     else:
-        # svm_model = SVC(kernel='sigmoid', C=50, gamma=0.01, random_state=0, probability=True)
-        # svm_model = SVC(kernel='linear', C=0.5, gamma='scale', random_state=0, probability=True)
-        svm_model = SVC(kernel='rbf', C=50, gamma=0.005, random_state=0, probability=True)
-        # svm_model = SVC(kernel='sigmoid', C=1000.0, gamma=0.005, random_state=0, probability=True)
-    svm_model.fit(images, labels)
+        if settings.USE_FACENET:
+            svm_model = SVC(kernel='rbf', C=10, gamma=0.01, random_state=0, probability=True)  # mask 92%
+            # svm_model = SVC(kernel='linear', C=0.5, gamma='scale', random_state=0, probability=True) # 100%
+        else:
+            # svm_model = SVC(kernel='rbf', C=50, gamma=0.005, random_state=0, probability=True)  # 95%
+            svm_model = SVC(kernel='sigmoid', C=50, gamma=0.01, random_state=0, probability=True) # mask 67%
+            # svm_model = SVC(kernel='linear', C=0.5, gamma='scale', random_state=0, probability=True) # 94%
+    svm_model.fit(features, labels)
     course_directory = get_course_models_directory(course_code)
     model_name = f"{semester_code}.joblib"
     model_path = path.join(course_directory, model_name)
     joblib.dump(svm_model, model_path)
     print('--- MODEL CREATED ---')
+    create_scatter_plot(features, labels)
     return model_path
 
 
 def validate_model(semester_code: str, course_code: str, save_preprocessing=False):
+    print('--- PREPARING TESTING DATASETS ---')
     validation_images, validation_labels = prepare_datasets(DatasetType.VALIDATION, save_preprocessing)
     print('--- TESTING MODEL ---')
     course_directory = get_course_models_directory(course_code)
