683651f314e3c5e47a1a7f0d720016b7d17376eb
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2022-02-17 21:13:26 +0800

    assets: update get result

--- app/api/endpoints/assets.py
+++ app/api/endpoints/assets.py
@@ -1,9 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi.responses import FileResponse
+from sqlalchemy.orm import Session
 
+from app import crud
+from app.api import deps
+from app.models import schemas
 from app.resources.enums import DatasetType
 from app.utils.file_helper import get_avatar_file, get_user_dataset_file, get_result_file, get_list_files, \
-    get_user_datasets_directory, get_user_dataset_raw_file
+    get_user_datasets_directory, get_user_dataset_raw_file, get_meeting_results_directory
 
 router = APIRouter()
 
@@ -39,7 +43,10 @@ def get_raw_dataset(dataset_type: DatasetType, username: str, file_name: str):
     return FileResponse(dataset)
 
 
-@router.get("/result/{file_name}")
-def get_result(file_name: str):
-    result = get_result_file(file_name)
+@router.get("/result/{meeting_id}/{file_name}")
+def get_result(meeting_id: int, file_name: str,
+               current_semester: schemas.Semester = Depends(deps.get_active_semester),
+               db: Session = Depends(deps.get_db)):
+    meeting = crud.meeting.get(db, meeting_id)
+    result = get_result_file(current_semester.code, meeting.course.code, meeting_id, file_name)
     return FileResponse(result)
