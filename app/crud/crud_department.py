15dd6de0b5c26f1fcb2c6d586210b8db06e35527
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2022-03-11 20:47:46 +0800

    crud_department : add function get by username parsing

--- app/crud/crud_department.py
+++ app/crud/crud_department.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 
 from sqlalchemy.orm import Session
 
@@ -8,8 +8,12 @@ from app.models.schemas.department import DepartmentCreate, DepartmentUpdate
 
 
 class CRUDDepartment(CRUDBase[Department, DepartmentCreate, DepartmentUpdate]):
-    def get_by_faculty_id(self, db: Session, faculty_id: int) -> Optional[Department]:
+    def get_by_faculty_id(self, db: Session, faculty_id: int) -> List[Department]:
         return db.query(Department).filter(Department.faculty_id == faculty_id).all()
+
+    def get_by_username_parsing(self, db: Session, username: str) -> Optional[Department]:
+        code = username[:3] if len(username) > 3 else ""
+        return db.query(Department).filter(Department.code == code).first()
 
 
 department = CRUDDepartment(Department)
