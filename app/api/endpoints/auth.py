200220953f50ee8d3923d48da655bb1cc7113aec
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2022-03-23 22:58:10 +0800

    auth : add feature to login using neosia student account

--- app/api/endpoints/auth.py
+++ app/api/endpoints/auth.py
@@ -1,11 +1,13 @@
 from typing import Any, Union
 
+import httpx
 from fastapi import APIRouter, Depends, HTTPException, status, Form
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from app import crud
 from app.core.auth import auth
+from app.core.config import settings
 from app.models import domains, schemas
 
 from app.api import deps
@@ -20,9 +22,35 @@ def login_access_token(
 ) -> Any:
     user = crud.user.authenticate(db, username=form_data.username, password=form_data.password)
     if not user:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect username or password")
+        with httpx.Client() as client:
+            headers = {
+                'token': settings.NEOSIA_API_HEADER_TOKEN,
+                'Content-Type': "application/x-www-form-urlencoded",
+            }
+            data = {
+                "username": form_data.username,
+                "password": form_data.password
+            }
+            response = client.post(settings.NEOSIA_API_AUTH_STUDENT, data=data, headers=headers)
+            result = response.json()['success']
+            if result == "1":
+                user_local = crud.user.get_by_username(db, username=form_data.username)
+                if user_local:
+                    user = user_local
+                else:
+                    department = crud.department.get_by_username_parsing(form_data.username)
+                    user_in = schemas.UserStudentCreate(
+                        name=form_data.username,
+                        username=form_data.username,
+                        password=form_data.password,
+                        student=schemas.StudentCreate(department_id=department.id)
+                    )
+                    user = crud.user.create_student(db, obj_in=user_in)
+            else:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect username or password")
     elif not crud.user.is_active(user):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User is inactive")
+
     token = schemas.Token(
         access_token=auth.encode_token(user.username),
         refresh_token=auth.encode_refresh_token(user.username),
