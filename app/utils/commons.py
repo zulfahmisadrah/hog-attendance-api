89d484b0987c2ee220d3b2ce0e595ac96b8e83ef
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2022-04-18 21:59:57 +0800

    commons : create parse year from username

--- app/utils/commons.py
+++ app/utils/commons.py
@@ -5,3 +5,11 @@ def get_current_datetime(str_format="%Y%m%d_%H%M%S") -> str:
     now = datetime.now()
     str_current_datetime = now.strftime(str_format)
     return str_current_datetime
+
+
+def parse_year_from_username(username: str) -> int:
+    code = username[4:6] if len(username) > 6 else ""
+    current_year = get_current_datetime("%Y")
+    current_year_code = current_year[:2]
+    year = int(current_year_code + code)
+    return year
