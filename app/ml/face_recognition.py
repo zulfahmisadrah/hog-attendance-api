14ec791f9db2c68843a2d41d17118eefa9c6289b
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2022-05-22 20:23:02 +0800

    face_recognition: clean up code

--- app/ml/face_recognition.py
+++ app/ml/face_recognition.py
@@ -53,25 +53,14 @@ def recognize(db: Session, face_image, semester_code: str, course_code: str, sav
             hog_path = path.join(preprocessed_images_dir, f"{file_name_prefix}.7_hog.jpg")
             cv2.imwrite(hog_path, hog_image * 255.)
 
-    # pred = svm_model.predict(hog_desc.reshape(1, -1))
-    # print("hog_desc", [hog_desc])
     course_directory = get_course_models_directory(course_code)
     model_name = f"{semester_code}.joblib"
     model_path = path.join(course_directory, model_name)
-    # svm_model = pickle.load(open(model_path, 'rb'))
     svm_model = joblib.load(model_path)
     pred = svm_model.predict([feature])
     results = svm_model.predict_proba([feature])[0]
-    # print(svm_model.classes_)
-    # print(results)
-    # print(zip(svm_model.classes_, results))
     prob_per_class_dictionary = dict(zip(svm_model.classes_, results))
-    # print(prob_per_class_dictionary)
-    print(pred)
+    results_ordered_by_probability = sorted(zip(svm_model.classes_, results), key=lambda x: x[1], reverse=True)
     if pred:
-        print("PROBABILITY: ", prob_per_class_dictionary[pred[0]])
-    # results_ordered_by_probability = list(map(lambda x: x[0],
-    #                                      sorted(zip(svm_model.classes_, results), key=lambda x: x[1], reverse=True)))
-    # print(results_ordered_by_probability)
     return pred[0]
 
